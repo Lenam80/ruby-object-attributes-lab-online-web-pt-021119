b696a35abb6db9ebd17e77427290a701bd944598
Angelina Montanez <angelinamontanez80@gmail.com>
2019-02-27 20:40:34 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  def initialize=(breed)
+   @breed = Beagle
+  end
+
+
+  def breed=(breed)
+  @breed =breed
+  end
+
+
+  def name=(name)
+    @name =name
+  end
+
+  def name
+    @name
+  end
+end
